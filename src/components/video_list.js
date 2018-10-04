a3d859705ce4c6c869d3e150013e88b0e71e724e
Cutlass <kwonarseus@naver.com>
2018-10-04 19:47:09 +0900

    add videodetail & onclick event

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-  const VideoItems = props.videos.map(video => <VideoListItem key={video.etag} video={video} />);
+  const VideoItems = props.videos.map(video => (
+    <VideoListItem
+      onVideoSelect={props.onVideoSelect}
+      key={video.etag}
+      video={video}
+    />
+  ));
 
   return (
     <ul className="col-md-4 list-group">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YoutubeAPI from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAXIvFh2IvnjXnLQVnnw1UTrDJRCJIX36g';
 
@@ -10,10 +11,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YoutubeAPI({ key: API_KEY, term: 'skateboard' }, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -21,7 +28,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+        />
       </div>
     );
   }
